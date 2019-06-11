f7ae4363442129b8d8b80b068a16d1ab1e91fb3d
aarriiel <aarriiel@gmail.com>
2019-06-11 14:12:10 +0800

    fix_avatar_ms

--- DingDong/app/src/main/java/com/example/aesophor/dingdong/ui/settings/SettingsFragment.java
+++ DingDong/app/src/main/java/com/example/aesophor/dingdong/ui/settings/SettingsFragment.java
@@ -1,12 +1,15 @@
 package com.example.aesophor.dingdong.ui.settings;
 
-import android.app.Activity;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.content.Intent;
-import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.support.annotation.*;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +17,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.widget.*;
 import com.example.aesophor.dingdong.MessengerActivity;
 import com.example.aesophor.dingdong.R;
 import com.example.aesophor.dingdong.network.Response;
 import com.example.aesophor.dingdong.user.User;
 import com.example.aesophor.dingdong.util.ImageUtils;
-import android.provider.MediaStore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class SettingsFragment extends Fragment {
+
     public static final int PICK_IMAGE = 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +54,7 @@ public class SettingsFragment extends Fragment {
         final EditText passwordField = activity.findViewById(R.id.passwordField);
         Button updateButton = activity.findViewById(R.id.updateButton);
         Button avatarButton = activity.findViewById(R.id.avatarButton);
-        ImageView avatarSet = activity.findViewById(R.id.avatarSet);
+        final ImageView avatarSet = activity.findViewById(R.id.avatarSet);
 
 
         final User currentUser = activity.getUser();
@@ -49,23 +62,31 @@ public class SettingsFragment extends Fragment {
         fullnameField.setText(currentUser.getFullname());
         ImageUtils.b64LoadImage(avatarSet, currentUser.getB64Avatar());
 
-        avatarButton.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                getIntent.setType("image/*");
-                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                pickIntent.setType("image/*");
-                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
-                startActivityForResult(chooserIntent, PICK_IMAGE);
+
+        avatarButton.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
             }
         });
 
         updateButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Response update = currentUser.update(fullnameField.getText().toString(), passwordField.getText().toString());
+                // Encode avatar bitmap to base64
+                MessengerActivity activity = (MessengerActivity) getContext();
+                final ImageView avatarSet = activity.findViewById(R.id.avatarSet);
+                BitmapDrawable drawable = (BitmapDrawable) avatarSet.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream .toByteArray();
+                String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+                Response update = currentUser.update(fullnameField.getText().toString(), passwordField.getText().toString(), encoded);
                 if (update.success()) {
                     new AlertDialog.Builder(getContext())
                             .setTitle("Success")
@@ -82,12 +103,25 @@ public class SettingsFragment extends Fragment {
             }
         });
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == PICK_IMAGE) {
-                /*Bitmap photo = data.getParcelableExtra("data");
-                avatarSer.setImageBitmap(photo);*/
-            }
+        switch (requestCode) {
+            case PICK_IMAGE:
+                Uri returnUri = data.getData();
+                Bitmap bitmapImage = null;
+                try {
+                    bitmapImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), returnUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                MessengerActivity activity = (MessengerActivity) getContext();
+                final ImageView avatarSet = activity.findViewById(R.id.avatarSet);
+                avatarSet.setImageBitmap(bitmapImage);
+                break;
+            default:
+                break;
         }
-
     }
+
+}
